a0c034f432592897f378bfcb4d7e4d37ff771876
isadora <isadora.benner@gmail.com>
2025-04-03 13:12:19 -0300

    Inserindo fundo da tela de login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,24 +1,33 @@
 <template>
-    <div>
-        <!-- PODE USAR ESSA DIV DE BASE SE QUISER -->
+  <div class="background-container">
+    <div class="header-content">
+      <img src="@/assets/fundo-login.png" alt="Tecnospeed" class="fundo">
     </div>
+  </div>
 </template>
-
-<script>
-export default {
-  name: 'LoginView',
-  data() {
-    return {
-    
-    }
-  },
-  methods: {
-
-  }
-}
-
-</script>
 
 <style scoped>
 
+.background-container {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f5f5; 
+}
+
+.fundo {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain; 
+  display: block;
+}
 </style>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
