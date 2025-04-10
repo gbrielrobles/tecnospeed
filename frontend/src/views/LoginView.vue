1c6403d4a8c0470bfc89dff68cc023fd076e97a8
isadrgb <isadora.benner@gmail.com>
2025-04-10 12:50:54 -0300

    ponto flutuante da tela de login

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="background-container">
-    <div class="header-content">
-      <img src="@/assets/fundo-login.png" alt="Tecnospeed" class="fundo">
+    <img src="@/assets/fundo-login.png" alt="Fundo" class="fundo" />
+
+    <div class="form-container">
+      <div class="form-box">
+        <img src="@/assets/logo.png" alt="tecnospeed" class="logo" />
+        <h1 class="title">Bem-vindo</h1>
+        <p class="subtitle">Para criar uma nova Carta de VAN</p>
+
+        <input 
+          type="text" 
+          v-model="cnpj"
+          placeholder="Inserir CNPJ" 
+          class="input"
+        />
+        
+        <input 
+          type="text" 
+          v-model="token"
+          placeholder="Inserir Token" 
+          class="input"
+        />
+        
+        <button class="button" @click="handleLogin">
+          Criar
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-
+<style>
 .background-container {
+  position: relative;
   height: 100vh;
   width: 100vw;
-  overflow: hidden; 
+  overflow: hidden;
+}
+
+.fundo {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+
+.form-container {
+  height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -24,10 +62,17 @@
   object-fit: contain; 
   display: block;
 }
+
 </style>
 
-<script>
-export default {
-  name: 'LoginView'
+<script setup>
+import { ref } from 'vue'
+
+const cnpj = ref('')
+const token = ref('')
+
+function handleLogin() {
+  console.log('CNPJ:', cnpj.value)
+  console.log('Token:', token.value)
 }
 </script>
