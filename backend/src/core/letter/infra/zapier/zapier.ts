09a3083367cf46241130c23071793430ca314b4e
viniciusmaier <viniciusfiel1000@gmail.com>
2025-05-28 20:05:50 -0300

    wip

--- backend/src/core/letter/infra/zapier/zapier.ts
+++ backend/src/core/letter/infra/zapier/zapier.ts
@@ -5,15 +5,19 @@ import { Injectable } from "@nestjs/common";
 export class ZapierService {
     constructor(private readonly httpservice: HttpService){}
 
-    sendFile(file: any) {
+    sendFile(file: Base64URLString, client: {documentClient: string; product: any; documentSH: string; email: string}) {
         this.httpservice.postForm<any, {
             cnpj_SH: string,
             email: string,
-            arquivo: string,
+            arquivo: Base64URLString,
             CNPJ_cliente: string,
             Produto:string,
         }>('https://hooks.zapier.com/hooks/catch/21923307/2gwb3a6/', {
-            
+           arquivo: file,
+           CNPJ_cliente: client.documentClient,
+           cnpj_SH: client.documentSH,
+           email: client.email,
+           Produto: client.product
         })
     }
 }
